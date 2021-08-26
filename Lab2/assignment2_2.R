e67f5a6b97c6ed64bf275aaf6a55727cbcc9052c
sangeeta1998 <sangeeta.kakati19@gmail.com>
2021-08-27 00:03:26 +0530

    Update assignment2_2.R

--- Lab2/assignment2_2.R
+++ Lab2/assignment2_2.R
@@ -15,12 +15,16 @@ colSums(iris[sapply(iris, is.numeric)])
 
 ### Question 2(d)
 # counting metrices of all the species
-# count number of c in each columnn
+Count <- sapply(df[,1:5], NROW)
+Count
+# count number of values in each columnn
 countedValues <- aggregate(x= df, by= list(df$Species), function(x) length(x))
 countedValues
 
 ### Question 2(e)
 # maximum of all the species
-# maximum of c in each column
-maxValues <- aggregate(x= df, by= list(df$Species), max)
+Maximum <- sapply(df[,1:5], max)
+Maximum
+# maximum value in each column
+maxValues <- aggregate(x= df[,1:5], by= list(df$Species), FUN= max)
 maxValues
