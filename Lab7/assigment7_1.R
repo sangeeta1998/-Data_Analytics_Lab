efd17bc98251f432ae2f753de46e1be16891b67c
sangeeta1998 <sangeeta.kakati19@gmail.com>
2021-10-29 14:32:27 +0530

    Update assigment7_1.R

--- Lab7/assigment7_1.R
+++ Lab7/assigment7_1.R
@@ -1,17 +1,30 @@
-install.packages("igraph")
-install.packages('expm')
-library(expm)
-library(igraph)
-g <-sample_gnm(4,m= 12, directed = TRUE,loops = 3)
-g
-plot(g)
-#using eigen vector
-adj_m <- t(as_adjacency_matrix(g, sparse = FALSE))
-adj_m_mod <- prop.table(adj_m, 2)
-lr <- eigen(adj_m_mod)$vectors[ , 1]
-lr <- Re(lr/sum(lr))
-matrix(lr, ncol = 1)
 
-#using teleporting factor 0.8
-df <- data.frame(pr = page_rank(g, damping = 0.8)$vector)
-df
+# Loading Libraries
+library(arules)
+library(arulesViz)
+library(RColorBrewer)
+
+# import dataset
+data("Groceries")
+
+# looking into the dataset
+View(Groceries)
+
+
+# Question 1(b):
+#  Apply apriori algorithm and mine the frequent itemsets and association rules. Use
+#  the default values of minimum support and minimum confidence.
+
+# using apriori() function
+rules <- apriori(Groceries,
+                 parameter = list(supp = 0.01, conf = 0.2))
+
+# using inspect() function
+inspect(rules[1:10])
+
+# using itemFrequencyPlot() function
+arules::itemFrequencyPlot(Groceries, topN = 20,
+                          col = brewer.pal(8, 'Pastel2'),
+                          main = 'Relative Item Frequency Plot',
+                          type = "relative",
+                          ylab = "Item Frequency (Relative)")
